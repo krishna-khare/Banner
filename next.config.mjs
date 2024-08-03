b24f9a78bb4aede746caa956c312f1f300e71c90
KRISHNA KHARE <102462298+krishna-khare@users.noreply.github.com>
2024-08-03 13:00:30 +0530

    next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,5 +3,5 @@ const isProd = process.env.NODE_ENV === 'production';
 
 export default {
   output: 'export',
-  assetPrefix: isProd ? '/your-repository-name/' : '',
+  assetPrefix: isProd ? '/banner/' : '',
 };
